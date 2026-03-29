b556915f71d56b7612f45db6cd337cf4864fac32
dharmi-sukhadiya <dharmisukhadiya@gmail.com>
2026-03-29 21:04:02 +0530

    add play functions

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("js")
+async function getsongs() {
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    // console.log(as)
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+    }
+    // console.log(songs)
+    return songs;
+}
+
+async function main(){
+    //get the list of all songs
+let songs = await getsongs()
+console.log(songs)
+
+//play the first song
+
+var audio = new Audio(songs[0]);
+audio.play();
+
+}
+
+main()
